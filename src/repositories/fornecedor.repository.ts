f7ea3eb0d805cdc2137059ae24953b1064d35e35
Thiago-Nassiben-Freri <thiagonassibenfreri2@gmail.com>
2026-05-06 17:27:17 -0300

    Criado arquivo fornecedor.repository.ts na pasta repositories

---
+++ src/repositories/fornecedor.repository.ts
@@ -0,0 +1,9 @@
+import { sqlite3 } from "sqlite3";
+import { db } from "../config/database";
+import { 
+    Fornecedor, 
+    EmailFornecedor, 
+    TelefoneFornecedor,
+    FornecedorCompleto
+} from "../models/fornecedor.model";
+
