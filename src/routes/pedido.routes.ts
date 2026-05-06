86658c20f0f18adea1c2f758a769576c45a9b6b3
GabrielVerdin <gabrielg.verdin@gmail.com>
2026-05-06 08:59:56 -0300

    implementa pedido.controller.ts

---
+++ src/routes/pedido.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import * as controller from "../controllers/pedido.controller";
+
+const router = Router();
+
+router.post("/", controller.criarPedido);
+router.get("/:id", controller.buscarPedidoPorId);
+router.put("/:id", controller.atualizarPedido);
+router.delete("/:id", controller.excluirPedido);
+
+export default router;
