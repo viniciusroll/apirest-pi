90473392bbc882b4fcf47185540a24bd146d3e45
Thiago-Nassiben-Freri <thiagonassibenfreri2@gmail.com>
2026-05-09 15:21:23 -0300

    add: Adicionado a função buscarFornecedorPorID no fornecedor.controller.ts

--- src/controllers/fornecedor.controller.ts
+++ src/controllers/fornecedor.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import * as service from "../services/fornecedor.service"
 import { criarFornecedorSchema, atualizarFornecedorSchema } from "../schemas/fornecedor.schema"
+import { number } from "zod/v4";
 
 
 export async function criarFornecedor(req: Request, res: Response) {
@@ -16,4 +17,10 @@ export async function criarFornecedor(req: Request, res: Response) {
 export async function listarFornecedores(req: Request, res: Response) {
     const fornecedores = await service.listarFornecedores();
     res.json(fornecedores);
+}
+
+export async function buscarFornecedorPorID(req: Request, res: Response) {
+    const fornecedor = await service.buscarFornecedorPorID(Number(req.params.id));
+    if (!fornecedor) return res.status(404).json({ erro: "Fornecedor não encontrado" });
+    res.json(fornecedor);
 }
