18f77741b73eb20df80cbb0125d8c1b6ab2b84e2
Thiago-Nassiben-Freri <thiagonassibenfreri2@gmail.com>
2026-05-09 15:12:09 -0300

    create: Criado fornecedor.controller.ts na pasta controllers

---
+++ src/controllers/fornecedor.controller.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from "express";
+import * as service from "../services/fornecedor.service"
+import { criarFornecedorSchema, atualizarFornecedorSchema } from "../schemas/fornecedor.schema"
+
+
