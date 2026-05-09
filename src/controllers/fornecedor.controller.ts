bc77c112a7ff073ecd8c1080e1d317b373b35001
Thiago-Nassiben-Freri <thiagonassibenfreri2@gmail.com>
2026-05-09 15:27:16 -0300

    add: Adicionado a função excluirFornecedor no fornecedor.controller.ts

--- src/controllers/fornecedor.controller.ts
+++ src/controllers/fornecedor.controller.ts
@@ -21,7 +21,7 @@ export async function listarFornecedores(req: Request, res: Response) {
 
 export async function buscarFornecedorPorID(req: Request, res: Response) {
     const fornecedor = await service.buscarFornecedorPorID(Number(req.params.id));
-    if (!fornecedor) return res.status(404).json({ erro: "Fornecedor não encontrado" });
+    if (!fornecedor) return res.status(404).json({ erro: "Fornecedor não encontrado!" });
     res.json(fornecedor);
 }
 
@@ -33,4 +33,10 @@ export async function atualizarFornecedor(req: Request, res: Response) {
     } catch(err: any) {
         res.status(400).json({ erro: err.message });
     }
+}
+
+export async function excluirFornecedor(req: Request, res: Response) {
+    const ok = await service.excluirFornecedor(Number(req.params.id));
+    if (!ok) return res.status(404).json({ erro: "Fornecedor não encontrado!" });
+    res.json({ mensagem: "Fornecedor excluído com sucesso" });
 }
