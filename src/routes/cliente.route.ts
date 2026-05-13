5684a45bbf62d70b23c05ed96c7ebf2a9e66a2d7
Leonardo Tagliamento Junior <leonardotagliamento@hotmail.com>
2026-05-13 19:28:56 -0300

    feat: Adicionar rota listarPedidosPorCliente

--- src/routes/cliente.route.ts
+++ src/routes/cliente.route.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import * as controller from "../controllers/cliente.controller";
+import * as pedidoController from "../controllers/pedido.controller";
 
 const router = Router();
 
 router.post("/", controller.criarCliente);
 router.get("/", controller.listarClientes);
 router.get("/:id", controller.buscarClientePorId);
+router.get("/:id/pedidos", pedidoController.listarPedidosPorCliente);
 router.put("/:id", controller.atualizarCliente);
 router.delete("/:id", controller.excluirCliente);
 
