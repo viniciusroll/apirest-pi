143f336ebb9fe9cd51ddcad2bab0e2e67b4bea3b
Leonardoalexanderxubento <leonardoleonardo1809@gmail.com>
2026-05-14 15:49:28 -0300

    feat: proteger todas as rotas com middleware JWT (RNF05)

--- src/routes/pedido.route.ts
+++ src/routes/pedido.route.ts
@@ -1,8 +1,11 @@
+
 import { Router } from "express";
 import * as controller from "../controllers/pedido.controller";
 import { authMiddleware } from "../middleware/auth.middleware";
 
 const router = Router();
+
+router.use(authMiddleware);
 
 router.post("/", authMiddleware, controller.criarPedido);
 router.get("/:id", controller.buscarPedidoPorId);
