6ff19327b0802bae999fda5c709dc61a62fcadc7
viniciusroll <viniciusrodrigo879@gmail.com>
2026-05-19 00:13:09 -0300

    inclui emails/telefones nos tipos de entrada de Fornecedor

--- src/models/fornecedor.model.ts
+++ src/models/fornecedor.model.ts
@@ -24,5 +24,16 @@ export interface FornecedorCompleto extends Fornecedor {
   telefones: string[];
 }
 
-export type EntradaCriarFornecedor = Omit<Fornecedor, "id_fornecedor" | "criado_em">;
+// Entrada de criacao: dados do fornecedor + os contatos multivalorados.
+// emails/telefones nao ficam na tabela 'fornecedor' (vao para
+// email_fornecedor / telefone_fornecedor), mas chegam juntos no body
+// da request, entao fazem parte do input do service/repository.
+export type EntradaCriarFornecedor = Omit<
+  Fornecedor,
+  "id_fornecedor" | "criado_em"
+> & {
+  emails: string[];
+  telefones: string[];
+};
+
 export type EntradaAtualizarFornecedor = Partial<EntradaCriarFornecedor>;
